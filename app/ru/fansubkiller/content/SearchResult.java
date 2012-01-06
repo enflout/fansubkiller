ca6b418f7b2ff1a2f3ad89e2a0f279f623c48734
enflout <enflout@gmail.com>
2012-01-06 19:55:22 +0400

    Избавляемся от даты добавления торрента, добавим позже.

--- app/ru/fansubkiller/content/SearchResult.java
+++ app/ru/fansubkiller/content/SearchResult.java
@@ -1,24 +1,15 @@
 package ru.fansubkiller.content;
 
-import java.util.Date;
-
 /**
  * @author i.orlov
  */
 public class SearchResult {
   private String name;
   private String href;
-  private Date added;
 
   public SearchResult(String name, String href) {
     this.name = name;
     this.href = href;
-  }
-
-  public SearchResult(String name, String href, Date added) {
-    this.name = name;
-    this.href = href;
-    this.added = added;
   }
 
   public String getName() {
@@ -35,14 +26,6 @@ public class SearchResult {
 
   public void setHref(String href) {
     this.href = href;
-  }
-
-  public Date getAdded() {
-    return added;
-  }
-
-  public void setAdded(Date added) {
-    this.added = added;
   }
 
   @Override
@@ -62,7 +45,7 @@ public class SearchResult {
       return false;
     }
     SearchResult sr = (SearchResult) o;
-    return this.name.equals(sr.name) && this.href.equals(sr.href);
+    return name.equals(sr.name) && href.equals(sr.href);
   }
 
   @Override
