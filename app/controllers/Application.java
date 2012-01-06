916c47a41f08b69f87a4ed1a9cff7fdcfad7750c
enflout <enflout@gmail.com>
2012-01-06 19:45:53 +0400

    Перенесли логику поиска из контроллера SearchController в контроллер Application.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,29 @@
 package controllers;
 
-import play.mvc.*;
+import play.mvc.Controller;
+import ru.fansubkiller.content.SearchResult;
+import ru.fansubkiller.search.ToshokanTracker;
+import ru.fansubkiller.search.Tracker;
+
+import models.Task;
+
+import java.util.List;
 
 public class Application extends Controller {
+
+  public static void index() {
+    List<Task> tasks = Tasks.getTasks();
+    render(tasks);
+  }
+
+  public static void search(String searchText) {
+  	List<Task> tasks = Tasks.getTasks();
+    if (searchText == null || "".equals(searchText)) {
+      render(tasks);
+    }
+    Tracker tracker = new ToshokanTracker();
+    List<SearchResult> results = tracker.search(searchText);
+    render(results, tasks);
+  }
 
 }
